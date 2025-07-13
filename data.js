567bf1465cb264f4815f8c93d632c4bd8cc31045
AzariaPlayer <azariaplayer@gmail.com>
2025-07-13 12:36:40 -0500

    matchy project - data and functions

--- data.js
+++ data.js
@@ -12,28 +12,47 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+var animal = {};
+// using dot notation add species as a property
+animal.species = 'cat';
+// using bracket notation add a property called name and give the animal a name 
+animal['name'] = 'Pickles';
+// use bracket notation to give a property called noises with a empty array 
+animal.noises = [];
+// print animal object to console
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+var noises = [];
+// using bracket notation give noises the first element 
+noises[0] = 'Meow';
+noises.push('Purr');
+noises.unshift('Hiss');
+noises.unshift('wiwiwi');
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// bracket notation and assign noises to animal to the noises array
+animal.noises = noises;
+// add another noise 
+animal.noises.push('aluhluhluh');
 
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ * - You can use either dot notation or bracket notation.
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * - You can only use bracket notation to access an element in an array.
  * *******************************************************************
  */
 
@@ -49,12 +68,66 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create a variable name animals and assign it an empty array 
+var animals = [];
+// push animal to animals 
+animals.push(animal);
+console.log(animals);
+// create a variable called duck 
+var duck = {
+  species: 'duck',
+  name: 'Jerome',
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+};
+// push duck to animals 
+animals.push(duck);
+console.log(animals);
+var bird = {
+  species: 'bird',
+  name: 'Woody',
+  noises: ['cawcaw', 'tweet', 'hoot', 'chirp']
+};
+animals.push(bird);
+console.log(animals);
+var dog = {
+  species: 'dog',
+  name: 'Susie',
+  noises: ['bark', 'whine', 'sniff', 'pant']
+};
+animals.push(dog);
+console.log(animals);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// will be using an array that way i can use bracket notation to access the indexes 
+var friends = []; 
+// create a function that takes the animal array and returns a random index of the input array
+// use Math.random()
+function getRandom(animals){
+  // make a variable to hold math.random - animals.length is the max it can choose from
+  let index = Math.floor(Math.random() * animals.length);
+  // return a random index from the input (animals)
+  return index;
+  
+}
+// friends.push(animals[3].name);
+/* 
+[ ] Using a random index from this function that you just created, get a random animal and add its name to friends.
+[ ] console.log friends.
+[ ] Using bracket notation, add the friends list as a property also named friends on one of the animals in the animals array
+
+*/
+
+
+friends.push(animals[getRandom(animals)].name);
+
+console.log(friends);
+animals[0].friends = friends;
+
+
+console.log(animals);
 
 
 
--- functions.js
+++ functions.js
@@ -12,26 +12,78 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function search(array, string){
+    // loop through each array index
+    for(var i = 0; i < array.length; i++){
+        // compare the indexs name and the string input
+        if(string === array[i].name){
+            // return the whole index if found
+            return array[i];
+        }  
+        }
+        // return outside of the loop
+        return null;
+    }
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function replace(animals, name, replacement){
+    // if an animal with the name inputed already exist replace the entire object with the replacement
+    // lets loop through the array first 
+    for(var i = 0; i < animals.length; i++){
+        if(name === animals[i].name){
+            // you want the animals[i] to now equal the replacement object 
+             animals[i] = replacement;
+             // break after it is done 
+             break;
+        }
+    }
+    // return the new object
+    return animals[i];
 
+    // else do nothing 
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(animals, name){
+    // rememeber the for loop
+    for(var i = 0; i < animals.length; i++){
+        if(name === animals[i].name){
+            // use splice to start with the index and only remove 1 
+           animals.splice(animals[i], 1);
+           
+        }
+    }
+    
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function add(animals, animal){
+    
+    for(var i = 0; i < animals.length; i++){
 
-
+        if(animals[i].name === animal.name){
+            return;
+        }
+        
+       
+    }
+    if(animal.species.length > 0 && animal.name.length > 0){   
+       return animals.push(animal);
+     }
+    
+}
+    
+// huh 
 
 /**
  * You did it! You're all done with Matchy!
